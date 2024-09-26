4170b5245fd40796e1fe25ee791579c90bd54f26
Habynner <habynner.jeuel@gmail.com>
2024-09-26 15:47:21 -0300

    Feat: put operators and aggregators on db querys, for matriculas.

--- src/controllers/CursosController.js
+++ src/controllers/CursosController.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize');
 const Controller = require('./Controller.js');
 const CursosServices = require('../services/CursoServices.js');
 
@@ -6,6 +7,23 @@ const cursosServices = new CursosServices();
 class CursosController extends Controller {
   constructor() {
     super(cursosServices);
+  }
+
+  async pegaCursos(req, res) {
+    const { data_inicial, data_final } = req.query;
+    const where = {};
+    //se existirem os param, criar uma prop {}
+    data_inicial || data_final ? where.data_inicio = {} : null;
+    // se existir data inicial, adiciona a prop gte com o valor
+    data_inicial ? where.data_inicio[Op.gte] = data_inicial : null;
+    // se existir data final, idem
+    data_final ? where.data_inicio[Op.lte] = data_final : null;
+    try {
+      const listaCursos = await cursosServices.pegaTodosOsRegistros(where);
+      return res.status(200).json(listaCursos);
+    } catch (erro) {
+      return res.status(500).json({ erro: erro.message });
+    }
   }
 }
 
